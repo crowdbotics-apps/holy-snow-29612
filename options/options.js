7c278886299c12c3bab316e55aaee2c81bd6fe3b
Crowdbotics <team@crowdbotics.com>
2021-08-07 15:18:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "holy_snow_29612Identifier",
+  url: "https://holy_snow_29612Identifier.botics.co",
+  api: "https://holy_snow_29612Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
